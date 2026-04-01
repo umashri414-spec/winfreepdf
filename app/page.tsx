44f19e16cb675675df33dbf3f0096c78230be7ca
uma <umashri414@gmail.com>
2026-04-01 14:55:06 +0530

    fix auth redirect

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
   const handleGoogleLogin = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: { redirectTo: window.location.origin },
+      options: { redirectTo: `${window.location.origin}/auth/callback`, },
     });
   };
 
